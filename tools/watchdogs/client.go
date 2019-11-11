e53511bd7055e066dba21e3c4179f96cb75572de
moyrn <2286167996@qq.com>
2019-11-11 18:42:56 +0800

    watchdogs rpc client 修改

--- tools/watchdogs/client.go
+++ tools/watchdogs/client.go
@@ -14,10 +14,11 @@ const (
 )
 
 type Client struct {
-	client *rpc.Client
+	client    *rpc.Client
+	container string
 }
 
-func NewRpcClient(serverIP string) (*Client, error) {
+func NewRpcClient(serverIP, container string) (*Client, error) {
 	router := ghq.New()
 	if err := router.LoadConfig(); err != nil {
 		return nil, err
@@ -31,28 +32,28 @@ func NewRpcClient(serverIP string) (*Client, error) {
 		return nil, err
 	}
 
-	return &Client{client: client}, nil
+	return &Client{client: client, container: container}, nil
 }
 
-func (c *Client) FeedDog(cont, key, context string) (result Message, err error) {
-	return c.call(cont, key, context, feedDogMethod)
+func (c *Client) FeedDog(key, context string) (result Message, err error) {
+	return c.call(key, context, feedDogMethod)
 }
 
-func (c *Client) DeadDog(cont, key string) (result Message, err error) {
-	return c.call(cont, key, "", deadDogMethod)
+func (c *Client) DeadDog(key string) (result Message, err error) {
+	return c.call(key, "", deadDogMethod)
 }
 
-func (c *Client) DeadReturn(cont, key string) (result Message, err error) {
-	return c.call(cont, key, "", deadReturnMethod)
+func (c *Client) DeadReturn(key string) (result Message, err error) {
+	return c.call(key, "", deadReturnMethod)
 }
 
-func (c *Client) Destroy(cont, key string) (result Message, err error) {
-	return c.call(cont, key, "", destroyMethod)
+func (c *Client) Destroy(key string) (result Message, err error) {
+	return c.call(key, "", destroyMethod)
 }
 
-func (c *Client) call(cont, key, context string, method string) (result Message, err error) {
+func (c *Client) call(key, context string, method string) (result Message, err error) {
 	if err := c.client.Call(container+method, DogReq{
-		Container: cont,
+		Container: c.container,
 		Key:       key,
 		Context:   context,
 	}, &result); err != nil {
